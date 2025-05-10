5fe65d1635fd7b3a14810632b6ab585aa03c5947
Kim Biggs <kim.biggs@syndigo.com>
2025-05-10 11:29:07 -0500

    Updated tests

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 test('buttonClickInEmail', async ({ page }) => {
   await page.goto('https://identity.hudl.com/u/login/identifier?state=hKFo2SBheTFpQkxEWWw1aGpsRzNRU3otQjlPR2stUUhZbUlmQqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIER4eWxuNU4tLUI1YVYzc21OTFhNdFJHQ2VjWU9TNW9so2NpZNkgbjEzUmZrSHpLb3phTnhXQzVkWlFvYmVXR2Y0V2pTbjU');
   await expect(page).toHaveTitle(/Log In/);
+  await percySnapshot(page, 'Hudl Login Page');
 });
