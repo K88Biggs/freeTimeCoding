7506759f51e2b014932b0163dfea78e7ae9990e9
Kim Biggs <kim.biggs@syndigo.com>
2025-05-10 11:32:26 -0500

    Updated tests

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import percySnapshot from '@percy/playwright';
 
 test('buttonClickInEmail', async ({ page }) => {
   await page.goto('https://identity.hudl.com/u/login/identifier?state=hKFo2SBheTFpQkxEWWw1aGpsRzNRU3otQjlPR2stUUhZbUlmQqFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIER4eWxuNU4tLUI1YVYzc21OTFhNdFJHQ2VjWU9TNW9so2NpZNkgbjEzUmZrSHpLb3phTnhXQzVkWlFvYmVXR2Y0V2pTbjU');
